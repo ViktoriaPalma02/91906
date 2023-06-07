7e4f1c013b430b96a6e52f7b834a0b425b078a49
Viktoria Palma <inobella023@gmail.com>
2023-06-08 07:31:01 +1200

    Ver 1.0 of code

--- 91906_Fakenewsdetector.py
+++ 91906_Fakenewsdetector.py
@@ -1,170 +1,109 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import Tk
+import tkinter as tk
 from tkinter import scrolledtext
 
-#When user agrees to terms, destroy TOS page and make main program available
+# When user agrees to terms, destroy TOS page and make main program available
 def destroy_TOS():
     TOS.destroy()
     user_text.configure(state="normal", bg="#E3E3E3")
     output_text.configure(bg="#E3E3E3")
 
-#make main window
+# Make main window
 menu = Tk()
 menu.title("Fake News Detector")
 menu.state('zoomed')
 menu.grid_columnconfigure(0, weight=1)
 menu.configure(bg="#EAE0D5")
 
-#Create title of page
-title_label = Label(menu, text="Fake News Detector", foreground="#22333B",bg="#EAE0D5", font=("Helvetica", 40, "bold"))
+# Create title of page
+title_label = Label(menu, text="Fake News Detector", foreground="#22333B", bg="#EAE0D5", font=("Helvetica", 40, "bold"))
 title_label.grid(padx=10, pady=10, column=0, row=0)
 
-#icon 
-icon = PhotoImage(file="assets\icon.png")
+# Icon
+icon = PhotoImage(file="assets/icon.png")
 icon = icon.subsample(2)
 icon_label = Label(menu, image=icon, bg='#EAE0D5')
 icon_label.grid(row=0, column=0, sticky=NW)
 
-#subheading
-file = open(r"text files\intro_text.txt", "r")
+# Subheading
+file = open(r"text files/intro_text.txt", "r")
 userinp_label = Label(menu, text=file.read(), font=("Helvetica", 10))
 userinp_label.grid(row=1, column=0, pady=5)
 
-#Create label frame 
-user_labelframe = LabelFrame(menu,pady=5, padx=5, bg="#5E503F")
+# Create label frame
+user_labelframe = LabelFrame(menu, pady=5, padx=5, bg="#5E503F")
 user_labelframe.grid(row=2, column=0, columnspan=4, padx=10, pady=10)
 user_labelframe.rowconfigure(0, weight=1)
 user_labelframe.columnconfigure(0, weight=1)
-user_labelframe.columnconfigure((0,1),weight=1)
+user_labelframe.columnconfigure((0, 1), weight=1)
 
-#Create label for instructions
-input_label = Label(user_labelframe, text="Input:",bg="#5E503F",fg="#FFFFFF", font=("bold"))
+# Create label for instructions
+input_label = Label(user_labelframe, text="Input:", bg="#5E503F", fg="#FFFFFF", font=("bold"))
 input_label.grid(row=0, column=0, padx=5, pady=5)
 
-output_label = Label(user_labelframe, text="Output:",bg="#5E503F",fg="#FFFFFF", font=("bold"))
+output_label = Label(user_labelframe, text="Output:", bg="#5E503F", fg="#FFFFFF", font=("bold"))
 output_label.grid(row=0, column=1, padx=5, pady=5)
 
-#user input and output
+# User input and output
 user_text = scrolledtext.ScrolledText(user_labelframe)
-user_text.grid(row=1,column=0,sticky=W)
+user_text.grid(row=1, column=0, sticky=NSEW)
 
 output_text = scrolledtext.ScrolledText(user_labelframe)
-output_text.grid(row=1,column=1,sticky=E)
+output_text.grid(row=1, column=1, sticky=NSEW)
 
 user_text.configure(state="disabled", bg="#898989")
 output_text.configure(state="disabled", bg="#898989")
 
-#Create TOS
+# Enter button for input
+enter_button = Button(menu, text="Enter", justify=CENTER)
+enter_button.grid(row=4, column=0, sticky=W, padx=20, pady=5)
+
+# Create TOS
 TOS = Toplevel(menu)
-TOS.geometry("500x200")
 TOS.title("Terms of Service")
-title_tos = Label(TOS, text="Terms of Service", font=("Courier",20, "bold"))
-title_tos.grid(row=0,column=0)
+TOS.geometry("500x400")
+TOS.transient(menu)  # Set the main window as the master for TOS
+TOS.lift()  # Raise TOS window above the main window
+TOS.grid_columnconfigure(0, weight=1)
 
-#make Labelframe
-tos_labelframe = LabelFrame(TOS, pady=5, padx=5)
-tos_labelframe.grid(row=1, column=0)
+title_tos = Label(TOS, text="Terms of Service", font=("Courier", 20, "bold"))
+title_tos.grid(row=0, column=0, sticky=NSEW, padx=10, pady=10)
 
-#open textfile and put in TOS
-f = open(r"text files\tos_text.txt", "r")
+# Create canvas for TOS
+tos_canvas = Canvas(TOS)#, width=tos_width)
+tos_canvas.grid(row=1, column=0, sticky=NSEW)
+
+# Create scrollbar for TOS
+tos_scrollbar = Scrollbar(TOS, orient=VERTICAL, command=tos_canvas.yview)
+tos_scrollbar.grid(row=1, column=1, sticky="ns")
+tos_canvas.configure(yscrollcommand=tos_scrollbar.set)
+
+# Make Labelframe inside canvas
+tos_labelframe = LabelFrame(tos_canvas)
+tos_labelframe.pack(fill="both", expand=True, pady=5,padx=5)
+
+# Open textfile and put in TOS
+f = open(r"text files/tos_text.txt", "r")
 content_tos = Label(tos_labelframe, text=f.read())
-content_tos.grid(row=0, column=0, pady=5, padx=5)
+content_tos.pack(pady=5, padx=5)
 
-#Make scrollbar for TOS
+# Configure canvas to scrollable region
+tos_canvas.create_window((0, 0), window=tos_labelframe, anchor="nw")
+tos_canvas.configure(scrollregion=tos_canvas.bbox("all"))
 
-#Buttons at end of service
+# Configure canvas scrolling
+tos_labelframe.bind("<Configure>", lambda e: tos_canvas.configure(scrollregion=tos_canvas.bbox("all")))
+tos_canvas.bind_all("<MouseWheel>", lambda e: tos_canvas.yview_scroll(int(-1 * (e.delta / 120)), "units"))
+
+# Buttons at end of service
 agree_button = ttk.Button(tos_labelframe, text="Agree", command=destroy_TOS)
-agree_button.grid(row=1, column=0, sticky=W)
+#agree_button.grid(padx=5, sticky=W, row=2, column=0)
+agree_button.pack(side=LEFT, padx=5)
 
-disagree_button = Button(tos_labelframe, text="Disagree")
-disagree_button.place(x=380, y=300)
+disagree_button = ttk.Button(tos_labelframe, text="Disagree")
+#disagree_button.grid(padx=5, row=2, column=1, sticky=E)
+disagree_button.pack(side=RIGHT, padx=5)
 
-
-from tkinter import *
-from tkinter import ttk
-from tkinter import Tk
-from tkinter import scrolledtext
-
-#When user agrees to terms, destroy TOS page and make main program available
-def destroy_TOS():
-    TOS.destroy()
-    user_text.configure(state="normal", bg="#E3E3E3")
-    output_text.configure(bg="#E3E3E3")
-
-#make main window
-menu = Tk()
-menu.title("Fake News Detector")
-menu.state('zoomed')
-menu.grid_columnconfigure(0, weight=1)
-menu.configure(bg="#EAE0D5")
-
-#Create title of page
-title_label = Label(menu, text="Fake News Detector", foreground="#22333B",bg="#EAE0D5", font=("Helvetica", 40, "bold"))
-title_label.grid(padx=10, pady=10, column=0, row=0)
-
-#icon 
-icon = PhotoImage(file="assets\icon.png")
-icon = icon.subsample(2)
-icon_label = Label(menu, image=icon, bg='#EAE0D5')
-icon_label.grid(row=0, column=0, sticky=NW)
-
-#subheading
-file = open(r"text files\intro_text.txt", "r")
-userinp_label = Label(menu, text=file.read(), font=("Helvetica", 10))
-userinp_label.grid(row=1, column=0, pady=5)
-
-#Create label frame 
-user_labelframe = LabelFrame(menu,pady=5, padx=5, bg="#5E503F")
-user_labelframe.grid(row=2, column=0, columnspan=4, padx=10, pady=10)
-user_labelframe.rowconfigure(0, weight=1)
-user_labelframe.columnconfigure(0, weight=1)
-user_labelframe.columnconfigure((0,1),weight=1)
-
-#Create label for instructions
-input_label = Label(user_labelframe, text="Input:",bg="#5E503F",fg="#FFFFFF", font=("bold"))
-input_label.grid(row=0, column=0, padx=5, pady=5)
-
-output_label = Label(user_labelframe, text="Output:",bg="#5E503F",fg="#FFFFFF", font=("bold"))
-output_label.grid(row=0, column=1, padx=5, pady=5)
-
-#user input and output
-user_text = scrolledtext.ScrolledText(user_labelframe)
-user_text.grid(row=1,column=0,sticky=W)
-
-output_text = scrolledtext.ScrolledText(user_labelframe)
-output_text.grid(row=1,column=1,sticky=E)
-
-user_text.configure(state="disabled", bg="#898989")
-output_text.configure(state="disabled", bg="#898989")
-
-#Create TOS
-TOS = Toplevel(menu)
-TOS.geometry("500x200")
-TOS.title("Terms of Service")
-title_tos = Label(TOS, text="Terms of Service", font=("Courier",20, "bold"))
-title_tos.grid(row=0,column=0)
-
-#make Labelframe
-tos_labelframe = LabelFrame(TOS, pady=5, padx=5)
-tos_labelframe.grid(row=1, column=0)
-
-#open textfile and put in TOS
-f = open(r"text files\tos_text.txt", "r")
-content_tos = Label(tos_labelframe, text=f.read())
-content_tos.grid(row=0, column=0, pady=5, padx=5)
-
-#Make scrollbar for TOS
-
-#Buttons at end of service
-agree_button = Button(tos_labelframe, text="Agree", command=destroy_TOS)
-agree_button.grid(row=1, column=0, sticky=W)
-
-disagree_button = Button(tos_labelframe, text="Disagree")
-disagree_button.grid(row=1, column=1, sticky=E)
-
-#run mainloop
-menu.mainloop()
-
-#run mainloop
+# Run mainloop
 menu.mainloop()
--- evaluate.py
+++ evaluate.py
@@ -1,39 +1,54 @@
 import validators 
+import whois
 
-testbad = "This domain sucks"
-testgood = "This domain is good"
+#testbad = "This domain sucks"
+#testgood = "This domain is good"
 #Function to evaluate URL
 def ev_url(url_input):
     print("This is a url:", url_input)
-    try:
-        domain_info = validators.domain(url_input)
-        return True
-    except:
-        return False
-    
-    while domain_info is True:
-        print("PLEASE WORK")
-  #  if validators.domain(url_input):
-   #     print("This is a good domain")
-    #    #print("this is good domain")
-    #else:
-     #   print("this is a bad domain")
-      #  return testbad
 
-        
-    #True
+    #try:
+    domain = whois.whois(url_input)
+    #if domain == True:
+     #   print(domain)
+    print(domain)
+    #    return False
+    #except:
+     #   return True
+
+real = "this is a registered domain"
+notreal = "this is not a registered domain"
+
+def domain(url_input):
+    if validators.domain(url_input):
+        try:
+            return print("this is a registerd domain")
+        except:
+            return print("This is not a registerd domain")
+    else:
+        return print("This wont work")
+
 
 #Evaluate if user inputs URL
 print("Hi please input a URL")
 user_input = input()
-print(user_input)
+#print(user_input)
 
 validate_url = validators.url(user_input)
 if validate_url:    
-    print("Url")
-    ev_url(user_input)
+    print("This is a Url")
+    #a = domain(user_input)
+    a = ev_url(user_input)
+    #print(a)
+
+
+    #ev_url(user_input)
 else:
     print("not a url")
+    print("But we try anyways")
+    #domain = whois.whois(user_input)
+   # print(domain)
 
-
+#print(ev_url, user_input)
+#print("Placeholder")
 
